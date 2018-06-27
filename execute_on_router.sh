1aca8a41d3e5eb05936b40718370664d4c15106d
KB_tourettes <38875366+PooFuckCunt@users.noreply.github.com>
2018-06-27 14:00:28 +1000

    changing router rules
    echo "5" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_time_wait
    echo "16360" > /proc/sys/net/netfilter/nf_conntrack_max

--- execute_on_router.sh
+++ execute_on_router.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# replace cmd1 for the command to execute
-
+#see for details http://ivan.blogs.chimerical.com.au/post/2012/07/05/Overcoming-NAT-dropouts-with-BitTorrent-TP-Link-TD-W8960N.aspx
+#originally taken from above link script originaly written by https://gist.github.com/javikalsan/9786684
 host=192.168.1.1
 port=23
-user=myuser
-pass=mypass
-cmd1='ifconfig wl0 down'
-
+user=admin
+pass=admin
+cmd1='echo "16360" > /proc/sys/net/netfilter/nf_conntrack_max'
+cmd2='echo "5" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_time_wait'
 ( echo open ${host}
 sleep 1
 echo ${user}
@@ -15,4 +15,8 @@ echo ${pass}
 sleep 1
 echo ${cmd1}
 sleep 2
- ) | telnet
+echo ${cmd2}
+sleep 2
+) | telnet
+#for testing crontab jobs
+touch /tmp/.telrout
